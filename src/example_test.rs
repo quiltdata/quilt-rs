657e96c223e5fffbb91480bbdf84ca6996257dee
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-13 15:13:54 -0800

    Create example_test.rs

---
+++ src/example_test.rs
@@ -0,0 +1,32 @@
+// https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(smaller.can_hold(&larger));
+    }
+}
