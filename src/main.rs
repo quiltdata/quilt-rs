86a9d23d44fa31e64e1d629dfc20b8335be4d55c
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-13 12:48:07 -0800

    hello random_string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rand::Rng;
+
+fn random_string(length: usize) -> String {
+    let mut rng = rand::thread_rng();
+    let chars: Vec<char> = (0..length)
+        .map(|_| rng.gen_range('a'..'z') as char)
+        .collect();
+    chars.into_iter().collect()
+}
+
+ 
 fn main() {
-    println!("Hello, world!");
+    let s = random_string(10);
+    println!("Hello, {}!", s);
 }
