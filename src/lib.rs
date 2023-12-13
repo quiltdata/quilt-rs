5cba747c281dd8ad400df30ce3f0bb2901af279d
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-13 15:17:34 -0800

    fail Rectangle test

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,36 @@ pub fn random_string(length: usize) -> String {
         .collect();
     chars.into_iter().collect()
 }
+
+// https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+
+#[derive(Debug)]
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(smaller.can_hold(&larger));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use quilt_rs::random_string;
+use quilt_rs::Rectangle;
 
 fn main() {
     let s = random_string(10);
     println!("Hello, {}!", s);
+
+    let r = Rectangle {
+        width: 8,
+        height: 7,
+    };
+    println!("Rectangle: {:?}", r);
 }
