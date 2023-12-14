17581823c6f75b3a418fedd5b8160a7dcf9d1011
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-13 23:07:53 -0800

    installed_packages

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,13 @@ mod s3_utils;
 pub use api::LocalDomain;
 pub use api::Manifest;
 pub use api::ManifestHeader;
+
 pub use quilt::ManifestRow;
+pub use api::AvailablePackage;
 pub use quilt::InstalledPackage;
 pub use api::RemoteManifest;
 pub use api::S3PackageURI;
+pub use api::PackageLineage;
 
 pub use api::browse_remote_package;
 pub use api::browse_remote_manifest;
@@ -26,6 +29,18 @@ pub async fn manifest_from_uri(uri_string: String) -> Result<Manifest, Error> {
     assert!(manifest.rows.len() > 0);
     manifest.rows.len();
     Ok(manifest)
+}
+
+pub async fn installed_packages() -> Result<Vec<AvailablePackage>, String> {
+    let path_buf = std::env::current_dir().unwrap();
+    let local_domain = LocalDomain::new(path_buf);
+    let installed_packages: Vec<AvailablePackage> = list_installed_packages(local_domain.into())
+        .await
+        .expect("Failed to list installed packages");
+    println!("installed_packages: {:#?}", installed_packages);
+    assert!(installed_packages.len() > 0);
+    installed_packages.len();
+    Ok(installed_packages)
 }
 
 #[cfg(test)]
