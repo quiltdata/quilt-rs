51806ac1b8c0bc3f8fd5e91175dd9cb111698c20
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-16 20:12:16 -0800

    test_client_new

--- src/quilt4/client.rs
+++ src/quilt4/client.rs
@@ -31,7 +31,7 @@ pub struct Client {
 
 impl Client {
 
-    pub fn local_domain() -> LocalDomain {
+    pub fn domain3() -> LocalDomain {
         let cwd = std::env::current_dir().unwrap();
         LocalDomain::new(cwd)
     }
@@ -49,7 +49,7 @@ impl Client {
 
     pub async fn manifest3_from_uri(&self, uri_string: String) -> Result<Manifest, Error> {
         let uri = S3PackageURI::try_from(uri_string.as_str()).expect("Failed to parse URI");
-        let local = Client::local_domain().into();
+        let local = Client::domain3().into();
         let manifest: Manifest = browse_remote_package(local, uri)
             .await
             .expect("Failed to browse remote package");
@@ -98,18 +98,7 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_manifest3_from_uri() {
-        // Arrange
-        let client = Client::new().await;
-        let uri_string = "quilt+s3://quilt-example#package=akarve/test_dest".to_string();
-
-        // Act
-        let result = client.manifest3_from_uri(uri_string).await;
-
-        // Assert
-        assert!(result.is_ok());
-        let manifest = result.unwrap();
-        assert!(manifest.rows.len() > 0);
+    async fn test_client_new() {
+        assert!(Client::new().await.to_string().contains("Client"));
     }
-
 }
