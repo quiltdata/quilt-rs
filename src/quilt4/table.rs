0d5fd14db75776fddc389be4d2afc415ea257b87
Dima Ryazanov <dima@gmail.com>
2023-12-20 14:13:38 -0800

    Implement Table.read4

--- src/quilt4/table.rs
+++ src/quilt4/table.rs
@@ -2,15 +2,13 @@
 //! Table4 is a wrapper for arrow-rs's Table, the native Manifest format for quilt4.
 //! It uses UPath to transparently read and write to/from local and remote filesystems,
 //! and provides methods to read/write (decode/encode) quilt3's JSONL format
-//! 
+//!
 
 use arrow::error::ArrowError;
 use arrow::record_batch::RecordBatch;
+use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 
-use super::{
-    upath::UPath,
-    row4::Row4,
-};
+use super::{row4::Row4, upath::UPath};
 use serde::{Deserialize, Serialize};
 
 const HEADER_ROW: &str = ".";
@@ -18,23 +16,23 @@ const HEADER_ROW: &str = ".";
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Table {
     #[serde(skip)]
-    records: Option<RecordBatch>, // Vec<RecordBatch>? DataFusion?
+    records: Vec<RecordBatch>, // Vec<RecordBatch>? DataFusion?
     path3: Option<UPath>,
     path4: Option<UPath>,
 }
 
 impl Table {
-    pub async fn new(path: Option<UPath>) -> Self {
+    pub fn new(path: Option<UPath>) -> Self {
         Table {
-            records: None,
+            records: vec![],
             path3: None,
             path4: path.clone(),
         }
     }
     pub fn to_string(&self) -> String {
-        format!("Table({})", self.path4.as_ref().unwrap().to_string()) +
-        &format!("({:?})\n", self.path3) +
-        &format!("[\n{:?}\n]", self.records)
+        format!("Table({})", self.path4.as_ref().unwrap().to_string())
+            + &format!("({:?})\n", self.path3)
+            + &format!("[\n{:?}\n]", self.records)
     }
     // Read quilt3's JSONL format
     pub fn read3(&self) -> Result<Self, ArrowError> {
@@ -50,8 +48,28 @@ impl Table {
 
     // Read quilt4's Parquet format
     pub fn read4(&self) -> Result<Self, ArrowError> {
-        // Implementation goes here
-        unimplemented!()
+        let path = self
+            .path4
+            .as_ref()
+            .ok_or(ArrowError::NotYetImplemented("only path4 supported".into()))?
+            .file_path
+            .as_ref()
+            .ok_or(ArrowError::NotYetImplemented(
+                "only file_path supported".into(),
+            ))?;
+        let file = std::fs::File::open(&path)?;
+        let mut reader_stream = ParquetRecordBatchReaderBuilder::try_new(file)?.build()?;
+
+        let mut records = vec![];
+        while let Some(item) = reader_stream.next() {
+            records.push(item?);
+        }
+
+        Ok(Self {
+            records,
+            path3: self.path3.clone(),
+            path4: self.path4.clone(),
+        })
     }
 
     // Write quilt4's Parquet format
@@ -74,5 +92,21 @@ impl Table {
     pub fn list_names(&self) -> Vec<Row4> {
         // Implementation goes here
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use utils::local_uri_parquet;
+
+    use super::*;
+
+    #[test]
+    fn read_existing() {
+        let table = Table::new(Some(UPath::new(local_uri_parquet())));
+        let new_table = table.read4().unwrap();
+        dbg!(&new_table);
+        assert!(new_table.records.len() == 1);
+        assert!(new_table.records[0].num_rows() == 2);
     }
 }
--- tests/test_quilt4.rs
+++ tests/test_quilt4.rs
@@ -1,11 +1,12 @@
 use quilt_rs::*;
+use utils::local_uri_parquet;
 
 pub async fn make_manifest(path_name: String) -> Manifest4 {
     let up = UPath::new(path_name);
     let cl = Client::new();
     let dom = Domain::new(cl.clone(), up.clone()).await;
     let nam = Namespace::new(dom, up.clone()).await;
-    let tab = Table::new(Some(up.clone())).await;
+    let tab = Table::new(Some(up.clone())).read4().unwrap();
     let manifest = Manifest4::new(
         nam.clone(),
         tab.clone(),
@@ -16,7 +17,7 @@ pub async fn make_manifest(path_name: String) -> Manifest4 {
  
 #[tokio::test]
 async fn test_quilt4_manifest() {
-    let manifest = make_manifest("test".to_string()).await;
+    let manifest = make_manifest(local_uri_parquet()).await;
     // serialize manifest to Json using Serde
     let json = serde_json::to_string(&manifest).unwrap();
     assert!(json.contains("test"));
