d7b86409f9a4e783c334f0920ae74486139a4da2
Dima Ryazanov <dima@gmail.com>
2023-12-29 17:11:25 -0800

    Implement reading parquet from S3

--- src/quilt4/table.rs
+++ src/quilt4/table.rs
@@ -4,10 +4,19 @@
 //! and provides methods to read/write (decode/encode) quilt3's JSONL format
 //!
 
+use std::{
+    io::{Error, ErrorKind},
+    sync::Arc,
+};
+
 use arrow::error::ArrowError;
 use arrow::record_batch::RecordBatch;
-use parquet::arrow::ParquetRecordBatchStreamBuilder;
+use aws_sdk_s3::config::ProvideCredentials;
+use object_store::{aws::AmazonS3Builder, ObjectStore};
+use parquet::arrow::{async_reader::ParquetObjectReader, ParquetRecordBatchStreamBuilder};
 use tokio_stream::StreamExt;
+
+use crate::s3_utils::get_region_for_bucket;
 
 use super::{row4::Row4, upath::UPath};
 use serde::{Deserialize, Serialize};
@@ -53,21 +62,56 @@ impl Table {
             .path4
             .as_ref()
             .ok_or(ArrowError::NotYetImplemented("only path4 supported".into()))?;
-        let path = match upath {
-            UPath::Local(path) => path,
-            UPath::S3 { bucket: _, path: _ } => {
-                return Err(ArrowError::NotYetImplemented(
-                    "s3 not implemented yet".into(),
-                ))
-            }
-        };
-        let file = tokio::fs::File::open(&path).await?;
-        let mut reader_stream = ParquetRecordBatchStreamBuilder::new(file).await?.build()?;
 
         let mut records = vec![];
-        while let Some(item) = reader_stream.next().await {
-            records.push(item?);
-        }
+
+        match upath {
+            UPath::Local(path) => {
+                let file = tokio::fs::File::open(&path).await?;
+                let mut stream = ParquetRecordBatchStreamBuilder::new(file).await?.build()?;
+                while let Some(item) = stream.next().await {
+                    records.push(item?);
+                }
+            }
+            UPath::S3 { bucket, path } => {
+                let region = get_region_for_bucket(bucket)
+                    .await
+                    .map_err(|err| Error::new(ErrorKind::Other, err))?;
+
+                // TODO: Cache the credentials in s3_util or use s3_util's clients
+                // TODO: Return custom errors instead of abusing io::Error.
+                let sdk_config =
+                    aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+                let cp = sdk_config
+                    .credentials_provider()
+                    .ok_or(Error::new(ErrorKind::Other, "missing credentials"))?;
+                let creds = cp
+                    .provide_credentials()
+                    .await
+                    .map_err(|err| Error::new(ErrorKind::Other, err))?;
+
+                let s3 = AmazonS3Builder::new()
+                    .with_bucket_name(bucket)
+                    .with_region(region.to_string())
+                    .with_access_key_id(creds.access_key_id())
+                    .with_secret_access_key(creds.secret_access_key())
+                    .with_token(creds.session_token().unwrap_or_default())
+                    .build()
+                    .map_err(|err| Error::new(ErrorKind::Other, err))?;
+
+                let obj_meta = s3
+                    .head(path)
+                    .await
+                    .map_err(|err| Error::new(ErrorKind::Other, err))?;
+                let reader = ParquetObjectReader::new(Arc::new(s3), obj_meta);
+                let mut stream = ParquetRecordBatchStreamBuilder::new(reader)
+                    .await?
+                    .build()?;
+                while let Some(item) = stream.next().await {
+                    records.push(item?);
+                }
+            }
+        };
 
         Ok(Self {
             records,
@@ -106,11 +150,19 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn read_existing() {
+    async fn read_existing_local() {
         let table = Table::new(Some(UPath::parse(&local_uri_parquet()).unwrap()));
         let new_table = table.read4().await.unwrap();
-        dbg!(&new_table);
-        assert!(new_table.records.len() == 1);
-        assert!(new_table.records[0].num_rows() == 2);
+        assert_eq!(new_table.records.len(), 1);
+        assert_eq!(new_table.records[0].num_rows(), 2);
+    }
+
+    #[tokio::test]
+    async fn read_existing_s3() {
+        let url = "s3://udp-spec/.quilt/packages/122045ee6d96fd1cd8d1555e2d86e7e4a1699c05eeba9a555a4ea789004816abb592.parquet";
+        let table = Table::new(Some(UPath::parse(url).unwrap()));
+        let new_table = table.read4().await.unwrap();
+        assert_eq!(new_table.records.len(), 1);
+        assert_eq!(new_table.records[0].num_rows(), 3);
     }
 }
