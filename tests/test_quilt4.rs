d58e58e609c875e3b6dae6c0bc1cc1aa3198405c
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-01-02 17:02:18 -0800

    redid test

--- tests/test_quilt4.rs
+++ tests/test_quilt4.rs
@@ -1,19 +1,13 @@
 use quilt_rs::*;
 use utils::local_uri_parquet;
 
-pub async fn make_manifest(cl: &Client, up: UPath) -> Manifest4 {
-    let dom = Domain::new(cl, up.clone());
-    let ns = dom.get("manual/test").await.unwrap();
-    manifest
-}
- 
 #[tokio::test]
 async fn test_quilt4_manifest() {
     let path_name = local_uri_parquet();
     let up = UPath::parse(&path_name).unwrap();
     let cl = Client::new();
-    let manifest = make_manifest(&cl, up).await;
-    // serialize manifest to Json using Serde
-    let json = serde_json::to_string(&manifest).unwrap();
-    assert!(json.contains("test"));
+    let dom = Domain::new(&cl, up);
+    let manifest = dom.get_latest("manual/test").await.unwrap();
+    let result = manifest.to_string();
+    assert!(result.contains("test"));
 }
