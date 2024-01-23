181d6aa33be20db97f9e69b69d241d75647ec5d7
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-01-22 16:45:23 -0800

    use temp-dir

--- tests/test_local.rs
+++ tests/test_local.rs
@@ -1,12 +1,11 @@
 use quilt_rs::LocalDomain;
-use std::env;
-use std::path::PathBuf;
 use utils::{TEST_BUCKET, TEST_PACKAGE, TEST_HASH};
+use temp_dir::TempDir;
 
 #[tokio::test]
 async fn test_local_manifest() {
-    let temp_folder = env::temp_dir().join(PathBuf::from("quilt_rs"));
-    let loc = LocalDomain::new(temp_folder);
+    let temp_folder = TempDir::new().unwrap();
+    let loc = LocalDomain::new(temp_folder.path().to_path_buf());
     let remote_manifest = quilt_rs::RemoteManifest {
         bucket: TEST_BUCKET.to_string(),
         namespace: TEST_PACKAGE.to_string(),
