59f3116f3b0e087c88452ec002320cd330dfd2ba
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-03-13 16:57:57 -0700

    fail Metakey::Version

--- src/quilt/storage/dal.rs
+++ src/quilt/storage/dal.rs
@@ -11,6 +11,7 @@ use opendal::Result;
 /// - Write two multipart files concurrently
 
 pub async fn bucket_operator(bucket: &str) -> Result<Operator> {
+    println!("bucket_operator: {}", bucket);
     let mut builder = S3::default();
     builder.bucket(bucket);
     builder.region("us-east-1");
@@ -24,23 +25,40 @@ pub async fn bucket_operator(bucket: &str) -> Result<Operator> {
 mod tests {
     use super::*;
     use crate::utils::WRITE_BUCKET;
-    use opendal::EntryMode;
+    use opendal::{EntryMode, Metakey};
 
     #[tokio::test]
-    async fn test_s3_access() -> Result<()> {
-        let op = bucket_operator(WRITE_BUCKET).await?;
+    async fn test_s3_write() -> Result<()> {
+        const TEST_DIR: &str = "test_dal/";
+        const TEST_FILE: &str = "hello.txt";
+        let test_path = format!("{}{}", TEST_DIR, TEST_FILE);
+        const TEST_BYTES: &[u8] = b"Hello, World!";
 
-        op.write("hello.txt", "Hello, World!").await?;
+        let op = bucket_operator(WRITE_BUCKET).await?;
+        op.write(test_path.as_str(), TEST_BYTES).await?;
+
+        let entries = op.list_with(TEST_DIR).metakey(Metakey::Version).await?;
+        for entry in entries {
+            println!("entry: {:?}", entry.path());
+            let meta = entry.metadata().metakey();
+            println!("meta: {:?}", meta);
+        }
+
         let bs = op.read("hello.txt").await?;
-        let meta = op.stat("hello.txt").await?;
+
+        let meta = op.stat_with(path)
+        println!("{:?}", meta);
         let mode = meta.mode();
+        let version = meta.version();
         let length = meta.content_length();
 
         assert_eq!(bs.as_slice(), b"Hello, World!");
         assert_eq!(mode, EntryMode::FILE);
         assert_eq!(length, 13);
+        assert!(version.is_some());
+        assert_eq!(version.unwrap().len(), 32);
 
-        op.delete("hello.txt").await?;
+        // op.delete("hello.txt").await?;
 
         Ok(())
     }
