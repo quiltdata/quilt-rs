a644728742b9afb1f9ad765d17af3d97d915790c
Maksim Chervonnyi <mail@redmax.dev>
2024-03-14 19:44:43 +0100

    parse S3URI version and add tests for S3URI parser

--- src/quilt/storage/s3.rs
+++ src/quilt/storage/s3.rs
@@ -1,6 +1,6 @@
 use aws_sdk_s3::primitives::ByteStream;
 use tokio::io::AsyncReadExt;
-use url::Url;
+use url::{form_urlencoded, Url};
 
 use crate::Error;
 
@@ -33,7 +33,14 @@ impl TryFrom<&str> for S3Uri {
             .host_str()
             .ok_or(Error::S3Uri("missing bucket".to_string()))?;
         let key: String = parsed_url.path().chars().skip(1).collect();
-        let version = None; // FIXME
+        let version = match parsed_url.query() {
+            Some(query) => form_urlencoded::parse(query.as_bytes())
+                .into_owned()
+                .collect::<std::collections::HashMap<_, _>>()
+                .get("version")
+                .cloned(),
+            None => None,
+        };
         Ok(Self {
             bucket: bucket.to_string(),
             key,
@@ -103,3 +110,70 @@ pub async fn put_object_contents(
 //     // TODO: support versioning?
 //     self.buckets.get(&uri.bucket).ok_or("bucket not found")?.get(&uri.key).ok_or(String::from("key not found")).cloned()
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_icorrect_scheme() -> Result<(), Error> {
+        let uri = S3Uri::try_from("https://bucket/foo/bar");
+        assert_eq!(
+            uri.unwrap_err().to_string(),
+            "Invalid URI scheme: Expected s3:// scheme".to_string(),
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_no_bucket() -> Result<(), Error> {
+        let uri = S3Uri::try_from("s3://");
+        assert_eq!(
+            uri.unwrap_err().to_string(),
+            "Invalid S3 URI: missing bucket".to_string(),
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_unversioned_uri() -> Result<(), Error> {
+        let uri = S3Uri::try_from("s3://bucket/foo/bar")?;
+        assert_eq!(
+            uri,
+            S3Uri {
+                bucket: "bucket".to_string(),
+                key: "foo/bar".to_string(),
+                version: None,
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_versioned() -> Result<(), Error> {
+        let uri = S3Uri::try_from("s3://bucket/foo/bar?version=abc")?;
+        assert_eq!(
+            uri,
+            S3Uri {
+                bucket: "bucket".to_string(),
+                key: "foo/bar".to_string(),
+                version: Some("abc".to_string()),
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_incorrect_query() -> Result<(), Error> {
+        let uri = S3Uri::try_from("s3://bucket/foo/bar?another=query")?;
+        assert_eq!(
+            uri,
+            S3Uri {
+                bucket: "bucket".to_string(),
+                key: "foo/bar".to_string(),
+                version: None,
+            }
+        );
+        Ok(())
+    }
+}
