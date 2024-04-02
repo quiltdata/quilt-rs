c6173abbb5995f3b28a5e2f172a050408ed401b7
Maksim Chervonnyi <mail@redmax.dev>
2024-04-02 16:24:09 +0200

    split logic to self-descriptive functions

--- src/quilt/flow/browse.rs
+++ src/quilt/flow/browse.rs
@@ -1,19 +1,64 @@
-use std::collections::BTreeMap;
 use std::path::PathBuf;
 
 use arrow::error::ArrowError;
 use aws_sdk_s3::error::SdkError;
-use multihash::Multihash;
-use tokio::fs;
-use tokio::io::AsyncReadExt;
+use tokio::{fs, io::AsyncReadExt};
 
 use crate::quilt::{
-    manifest,
+    manifest::Manifest,
     manifest_handle::{CachedManifest, ReadableManifest, RemoteManifest},
     storage, Error,
 };
-use crate::quilt4::table::HEADER_ROW;
-use crate::{paths, Row4, Table, UPath};
+use crate::{paths, Table, UPath};
+
+async fn is_parquet(client: &aws_sdk_s3::Client, manifest: &RemoteManifest) -> Result<bool, Error> {
+    match client
+        .head_object()
+        .bucket(&manifest.bucket)
+        .key(paths::get_manifest_key(&manifest.hash))
+        .send()
+        .await
+    {
+        Ok(_) => Ok(true),
+        Err(SdkError::ServiceError(err)) if err.err().is_not_found() => Ok(false),
+        Err(err) => Err(Error::S3(err.to_string())),
+    }
+}
+
+async fn fetch_parquet(
+    client: &aws_sdk_s3::Client,
+    manifest: &RemoteManifest,
+) -> Result<Vec<u8>, Error> {
+    let result = client
+        .get_object()
+        .bucket(&manifest.bucket)
+        .key(paths::get_manifest_key(&manifest.hash))
+        .send()
+        .await
+        .map_err(|err| Error::S3(aws_sdk_s3::error::DisplayErrorContext(err).to_string()))?;
+    let mut contents = Vec::new();
+    result
+        .body
+        .into_async_read()
+        .read_to_end(&mut contents)
+        .await?;
+    Ok(contents)
+}
+
+async fn fetch_jsonl(
+    client: &aws_sdk_s3::Client,
+    manifest: &RemoteManifest,
+) -> Result<Table, Error> {
+    let result = client
+        .get_object()
+        .bucket(&manifest.bucket)
+        .key(paths::get_manifest_key_legacy(&manifest.hash))
+        .send()
+        .await
+        .map_err(|err| Error::S3(aws_sdk_s3::error::DisplayErrorContext(err).to_string()))?;
+    let quilt3_manifest = Manifest::from_file(result.body.into_async_read()).await?;
+    Table::try_from(quilt3_manifest)
+}
 
 pub async fn cache_manifest(
     paths: &paths::DomainPaths,
@@ -29,7 +74,7 @@ pub async fn cache_manifest(
         .map(|_| cache_path)
 }
 
-// FIMXE: CachedManifest::browse(&RemoteManifest)
+// FIXME: CachedManifest::browse(&RemoteManifest)
 //        or RemoteManifest::browse -> CachedManifest
 //        or CachedManifest::try_from(RemoteManifest)
 pub async fn cache_remote_manifest(
@@ -48,75 +93,13 @@ pub async fn cache_remote_manifest(
     if !storage::fs::exists(&cache_path).await {
         // Does not exist yet
         let client = crate::s3_utils::get_client_for_bucket(&manifest.bucket).await?;
-
-        let result = client
-            .get_object()
-            .bucket(&manifest.bucket)
-            .key(paths::get_manifest_key(&manifest.hash))
-            .send()
-            .await;
-
-        match result {
-            Ok(output) => {
-                let mut contents = Vec::new();
-                output
-                    .body
-                    .into_async_read()
-                    .read_to_end(&mut contents)
-                    .await?;
-                storage::fs::write(&cache_path, &contents).await?;
-            }
-            Err(SdkError::ServiceError(err)) if err.err().is_no_such_key() => {
-                // Fallback: Download the JSONL manifest.
-                let result = client
-                    .get_object()
-                    .bucket(&manifest.bucket)
-                    .key(paths::get_manifest_key_legacy(&manifest.hash))
-                    .send()
-                    .await
-                    .map_err(|err| Error::S3(err.to_string()))?;
-
-                let quilt3_manifest =
-                    manifest::Manifest::from_file(result.body.into_async_read()).await?;
-                let header = Row4 {
-                    name: HEADER_ROW.into(),
-                    place: HEADER_ROW.into(),
-                    path: None,
-                    size: 0,
-                    hash: Multihash::default(),
-                    info: serde_json::json!({
-                        "message": quilt3_manifest.header.message,
-                        "version": quilt3_manifest.header.version,
-                    }),
-                    meta: match quilt3_manifest.header.user_meta {
-                        Some(meta) => meta.into(),
-                        None => serde_json::Value::Null,
-                    },
-                };
-                let mut records = BTreeMap::new();
-                for row in quilt3_manifest.rows {
-                    let mut info = row.meta.unwrap_or_default();
-                    let meta = info.remove("user_meta").unwrap_or_default();
-                    records.insert(
-                        row.logical_key.clone(),
-                        Row4 {
-                            name: row.logical_key,
-                            place: row.physical_key,
-                            path: None,
-                            size: row.size,
-                            hash: row.hash.try_into()?,
-                            info: info.into(),
-                            meta,
-                        },
-                    );
-                }
-                let table = Table { header, records };
-                table.write_to_upath(&UPath::Local(cache_path)).await?
-            }
-            Err(err) => {
-                return Err(Error::S3(err.to_string()));
-            }
-        }
+        if is_parquet(&client, &manifest).await? {
+            let output = fetch_parquet(&client, &manifest).await?;
+            storage::fs::write(&cache_path, &output).await?;
+        } else {
+            let table = fetch_jsonl(&client, &manifest).await?;
+            table.write_to_upath(&UPath::Local(cache_path)).await?;
+        };
     }
 
     Ok(CachedManifest {
