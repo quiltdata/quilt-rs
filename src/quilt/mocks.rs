488c6b4104da5e4c8651393d9e266014508bb83b
Maksim Chervonnyi <mail@redmax.dev>
2024-04-11 12:40:07 +0200

    more tests

--- src/quilt/mocks.rs
+++ src/quilt/mocks.rs
@@ -9,6 +9,15 @@ pub mod manifest {
     use crate::Row4;
     use crate::Table;
 
+    fn row4_with_name(name: String) -> Result<Row4, Error> {
+        Ok(Row4 {
+            name,
+            place: "s3://z/x/y?versionId=foo".to_string(),
+            hash: multihash::Multihash::wrap(345, b"Hello world")?,
+            ..Row4::default()
+        })
+    }
+
     pub fn default() -> impl ReadableManifest {
         struct InMemoryManifest {}
         impl ReadableManifest for InMemoryManifest {
@@ -27,15 +36,7 @@ pub mod manifest {
             async fn read(&self, _storage: &mut impl Storage) -> Result<Table, Error> {
                 let mut records = BTreeMap::new();
                 for key in &self.keys {
-                    records.insert(
-                        key.to_string(),
-                        Row4 {
-                            name: key.to_string(),
-                            place: "s3://z/x/y?versionId=foo".to_string(),
-                            hash: multihash::Multihash::wrap(345, b"Hello world")?,
-                            ..Row4::default()
-                        },
-                    );
+                    records.insert(key.to_string(), row4_with_name(key.to_string())?);
                 }
                 Ok(Table {
                     records,
