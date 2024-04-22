2dd3234e0ff48c4ea67a6de00752be05a80acc38
Maksim Chervonnyi <mail@redmax.dev>
2024-04-22 12:05:01 +0200

    bring back accidentally removed test files

--- src/utils.rs
+++ src/utils.rs
@@ -2,10 +2,9 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 
 pub static TEST_DOMAIN: &str = "fixtures/test_domain";
-pub static TEST_LOCAL_PARQUET: &str = ".quilt/packages/12201234.parquet";
-pub static TEST_LOCAL_PARQUET_CHECKSUMED: &str = ".quilt/packages/checksumed.parquet";
-pub static TEST_LOCAL_JSONL: &str =
-    ".quilt/packages/0428ab8c8b0fe83d9e57fb6b26ff190173caad00ed7aeb683ce26cc4b56ea4bb";
+pub static TEST_LOCAL_PARQUET: &str = "fixtures/manifest.parquet";
+pub static TEST_LOCAL_PARQUET_CHECKSUMMED: &str = "fixtures/checksummed.parquet";
+pub static TEST_LOCAL_JSONL: &str = "fixtures/manifest.jsonl";
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum TestFile {
@@ -18,14 +17,13 @@ pub enum TestFile {
 pub fn local_uri(key: TestFile) -> PathBuf {
     let files: HashMap<TestFile, &str> = HashMap::from([
         (TestFile::Parquet, TEST_LOCAL_PARQUET),
-        (TestFile::ParquetChecksummed, TEST_LOCAL_PARQUET_CHECKSUMED),
+        (TestFile::ParquetChecksummed, TEST_LOCAL_PARQUET_CHECKSUMMED),
         (TestFile::Json, TEST_LOCAL_JSONL),
         (TestFile::Domain, ""),
     ]);
 
     let cwd = std::env::current_dir().unwrap();
-    let domain = cwd.join(TEST_DOMAIN);
-    domain.join(files[&key])
+    cwd.join(files[&key])
 }
 
 pub fn local_uri_parquet() -> PathBuf {
