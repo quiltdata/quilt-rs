3d3643453dfa7daeed6f32e9f32e211aa81f107e
Maksim Chervonnyi <mail@redmax.dev>
2025-02-13 17:57:20 +0100

    split lineage.rs into main, package and status

--- src/lineage.rs
+++ src/lineage.rs
@@ -4,186 +4,30 @@
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 
-use multihash::Multihash;
-use serde::de::Error as DeserializeError;
 use serde::Deserialize;
-use serde::Deserializer;
 use serde::Serialize;
-use serde::Serializer;
 use tracing::log;
 
 #[cfg(test)]
 pub mod mocks;
 
 use crate::io::storage::Storage;
-use crate::manifest::Row;
-use crate::uri::ManifestUri;
 use crate::uri::Namespace;
 use crate::Error;
 use crate::Res;
 
-/// Describes modified states of a file
-#[derive(Debug, PartialEq)]
-pub enum Change {
-    // TODO: Use Row
-    Modified(PackageFileFingerprint), // modified to what
-    // TODO: Use Row
-    Added(PackageFileFingerprint), // added what
-    Removed(Row),                  // removed what
-}
+mod status;
+pub use status::Change;
+pub use status::ChangeSet;
+pub use status::InstalledPackageStatus;
+pub use status::PackageFileFingerprint;
+pub use status::UpstreamState;
 
-/// Map of all changed files
-pub type ChangeSet = BTreeMap<PathBuf, Change>;
-
-/// State of the local package relative to the remote package
-#[derive(Debug, PartialEq, Eq, Default, Serialize)]
-pub enum UpstreamState {
-    #[default]
-    UpToDate,
-    Behind,
-    Ahead,
-    Diverged,
-}
-
-impl From<PackageLineage> for UpstreamState {
-    fn from(lineage: PackageLineage) -> Self {
-        let behind = lineage.base_hash != lineage.latest_hash;
-        let ahead = lineage.base_hash != lineage.current_hash();
-        match (ahead, behind) {
-            (false, false) => Self::UpToDate,
-            (false, true) => Self::Behind,
-            (true, false) => Self::Ahead,
-            (true, true) => Self::Diverged,
-        }
-    }
-}
-
-/// Some auxiliary struct that we use instead of `Row` when the file is not yet commited
-#[derive(Clone, Debug, Default, PartialEq)]
-pub struct PackageFileFingerprint {
-    // FIXME: re-use Row
-    pub size: u64,
-    pub hash: Multihash<256>,
-}
-
-/// Status of the package and workding directory of the pakage
-#[derive(Debug, PartialEq, Default)]
-pub struct InstalledPackageStatus {
-    /// Current commit vs upstream state
-    pub upstream_state: UpstreamState,
-    /// File changes vs current commit
-    pub changes: ChangeSet,
-    // XXX: meta?
-}
-
-impl InstalledPackageStatus {
-    pub fn new(upstream_state: UpstreamState, changes: ChangeSet) -> Self {
-        Self {
-            upstream_state,
-            changes,
-        }
-    }
-}
-
-/// What is the latest commit and what are previous commits if present
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
-pub struct CommitState {
-    /// When the last commit was done
-    pub timestamp: chrono::DateTime<chrono::Utc>,
-    /// What is the hash of the latest commit
-    pub hash: String, // TODO: use multihash?
-    /// What are the previous comit hashes
-    #[serde(default = "Vec::new")]
-    pub prev_hashes: Vec<String>, // TODO: use multihashes?
-}
-
-/// State of the file tracked in lineage
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct PathState {
-    /// Last "modified" date.
-    /// Last time it was installed or commited.
-    pub timestamp: chrono::DateTime<chrono::Utc>,
-    /// Last tracked hash.
-    /// We don't track files modifications in real time.
-    /// We calculate hash when we commit or install file.
-    #[serde(
-        serialize_with = "multihash_to_str",
-        deserialize_with = "str_to_multihash"
-    )]
-    pub hash: Multihash<256>,
-}
-
-fn multihash_to_str<S: Serializer>(
-    hash: &Multihash<256>,
-    serializer: S,
-) -> Result<S::Ok, S::Error> {
-    let s = hex::encode(hash.to_bytes());
-    serializer.serialize_str(&s)
-}
-
-fn str_to_multihash<'de, D: Deserializer<'de>>(
-    deserializer: D,
-) -> Result<Multihash<256>, D::Error> {
-    let s = String::deserialize(deserializer)?;
-    let bytes = hex::decode(s).map_err(DeserializeError::custom)?;
-    Multihash::from_bytes(&bytes).map_err(DeserializeError::custom)
-}
-
-/// A map of paths to their state
-///
-/// The key is the name of the path, and the value is the state of the path
-pub type LineagePaths = BTreeMap<PathBuf, PathState>;
-
-/// Stores lineage (installation/modification history) of the package read from `data.json` file
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
-pub struct PackageLineage {
-    /// Local commits
-    pub commit: Option<CommitState>,
-    /// Where we intsalled this package from
-    pub remote: ManifestUri,
-    // TODO: I don't understand yet how and why we use it
-    pub base_hash: String,
-    /// Latest tracked hash. In other words, what was the remote hash when we last checked.
-    /// It can be different from the `remote.hash`, because we can install not the latest package.
-    pub latest_hash: String,
-    /// Installed paths (or files in other words)
-    #[serde(default = "BTreeMap::new")]
-    pub paths: LineagePaths,
-}
-
-impl PackageLineage {
-    pub fn from_remote(remote: ManifestUri, latest_hash: String) -> Self {
-        Self {
-            base_hash: remote.hash.clone(),
-            remote,
-            latest_hash,
-            commit: None,
-            paths: BTreeMap::new(),
-        }
-    }
-
-    pub fn current_hash(&self) -> &str {
-        self.commit.as_ref().map_or(&self.remote.hash, |c| &c.hash)
-    }
-
-    pub fn update_latest(&mut self, manifest_uri: ManifestUri) {
-        let new_latest_hash = manifest_uri.hash;
-        self.latest_hash.clone_from(&new_latest_hash);
-        self.base_hash.clone_from(&new_latest_hash);
-    }
-}
-
-impl From<ManifestUri> for PackageLineage {
-    fn from(uri: ManifestUri) -> Self {
-        Self {
-            base_hash: uri.hash.clone(),
-            remote: uri.clone(),
-            latest_hash: uri.hash.clone(),
-            commit: None,
-            paths: BTreeMap::new(),
-        }
-    }
-}
+mod package;
+pub use package::CommitState;
+pub use package::LineagePaths;
+pub use package::PackageLineage;
+pub use package::PathState;
 
 /// It's essentially just a map of `PackageLineage`.
 /// Represents the contents of `.quilt/data.json`
@@ -298,10 +142,12 @@ impl PackageLineageIo {
 mod tests {
     use super::*;
 
-    use crate::checksum::calculate_sha256_chunked_checksum;
-    use crate::mocks;
     use base64::prelude::BASE64_STANDARD;
     use base64::Engine;
+
+    use crate::checksum::calculate_sha256_chunked_checksum;
+    use crate::mocks;
+    use crate::uri::ManifestUri;
 
     #[test]
     fn test_syntax_error() {
---
+++ src/lineage/package.rs
@@ -0,0 +1,124 @@
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+
+use multihash::Multihash;
+use serde::de;
+use serde::ser;
+use serde::Deserialize;
+use serde::Serialize;
+
+use crate::lineage::status::UpstreamState;
+use crate::uri::ManifestUri;
+
+fn multihash_to_str<S: ser::Serializer>(
+    hash: &Multihash<256>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    let s = hex::encode(hash.to_bytes());
+    serializer.serialize_str(&s)
+}
+
+fn str_to_multihash<'de, D: de::Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Multihash<256>, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    let bytes = hex::decode(s).map_err(de::Error::custom)?;
+    Multihash::from_bytes(&bytes).map_err(de::Error::custom)
+}
+
+/// State of the file tracked in lineage
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PathState {
+    /// Last "modified" date.
+    /// Last time it was installed or commited.
+    pub timestamp: chrono::DateTime<chrono::Utc>,
+    /// Last tracked hash.
+    /// We don't track files modifications in real time.
+    /// We calculate hash when we commit or install file.
+    #[serde(
+        serialize_with = "multihash_to_str",
+        deserialize_with = "str_to_multihash"
+    )]
+    pub hash: Multihash<256>,
+}
+
+/// A map of paths to their state
+///
+/// The key is the name of the path, and the value is the state of the path
+pub type LineagePaths = BTreeMap<PathBuf, PathState>;
+
+/// What is the latest commit and what are previous commits if present
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub struct CommitState {
+    /// When the last commit was done
+    pub timestamp: chrono::DateTime<chrono::Utc>,
+    /// What is the hash of the latest commit
+    pub hash: String, // TODO: use multihash?
+    /// What are the previous comit hashes
+    #[serde(default = "Vec::new")]
+    pub prev_hashes: Vec<String>, // TODO: use multihashes?
+}
+
+/// Stores lineage (installation/modification history) of the package read from `data.json` file
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
+pub struct PackageLineage {
+    /// Local commits
+    pub commit: Option<CommitState>,
+    /// Where we intsalled this package from
+    pub remote: ManifestUri,
+    // TODO: I don't understand yet how and why we use it
+    pub base_hash: String,
+    /// Latest tracked hash. In other words, what was the remote hash when we last checked.
+    /// It can be different from the `remote.hash`, because we can install not the latest package.
+    pub latest_hash: String,
+    /// Installed paths (or files in other words)
+    #[serde(default = "BTreeMap::new")]
+    pub paths: LineagePaths,
+}
+
+impl From<PackageLineage> for UpstreamState {
+    fn from(lineage: PackageLineage) -> Self {
+        let behind = lineage.base_hash != lineage.latest_hash;
+        let ahead = lineage.base_hash != lineage.current_hash();
+        match (ahead, behind) {
+            (false, false) => Self::UpToDate,
+            (false, true) => Self::Behind,
+            (true, false) => Self::Ahead,
+            (true, true) => Self::Diverged,
+        }
+    }
+}
+
+impl PackageLineage {
+    pub fn from_remote(remote: ManifestUri, latest_hash: String) -> Self {
+        Self {
+            base_hash: remote.hash.clone(),
+            remote,
+            latest_hash,
+            commit: None,
+            paths: BTreeMap::new(),
+        }
+    }
+
+    pub fn current_hash(&self) -> &str {
+        self.commit.as_ref().map_or(&self.remote.hash, |c| &c.hash)
+    }
+
+    pub fn update_latest(&mut self, manifest_uri: ManifestUri) {
+        let new_latest_hash = manifest_uri.hash;
+        self.latest_hash.clone_from(&new_latest_hash);
+        self.base_hash.clone_from(&new_latest_hash);
+    }
+}
+
+impl From<ManifestUri> for PackageLineage {
+    fn from(uri: ManifestUri) -> Self {
+        Self {
+            base_hash: uri.hash.clone(),
+            remote: uri.clone(),
+            latest_hash: uri.hash.clone(),
+            commit: None,
+            paths: BTreeMap::new(),
+        }
+    }
+}
