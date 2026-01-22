7cebb89a1766c8fd3c331a903e89233634bc58c4
Maksim Chervonnyi <mail@redmax.dev>
2026-01-22 13:50:59 +0100

    extract hash re-calculationg

--- quilt-rs/src/checksum.rs
+++ quilt-rs/src/checksum.rs
@@ -38,11 +38,9 @@ pub enum ObjectHash {
     Crc64(Crc64Hash),
 }
 
-pub async fn verify_hash(file: File, hash: Multihash<256>) -> Res<Option<(u64, Multihash<256>)>> {
-    let file_metadata = file.metadata().await?;
-    let size = file_metadata.len();
-
-    let calculated_hash: Multihash<256> = match hash.code() {
+/// Refresh hash for a file using the same algorithm as the reference hash
+async fn refresh_hash(file: File, hash: &Multihash<256>) -> Res<Multihash<256>> {
+    match hash.code() {
         MULTIHASH_CRC64_NVME => Ok(Crc64Hash::from_file(file).await?.into()),
         MULTIHASH_SHA256 => Ok(Sha256Hash::from_file(file).await?.into()),
         MULTIHASH_SHA256_CHUNKED => Ok(Sha256ChunkedHash::from_file(file).await?.into()),
@@ -50,7 +48,14 @@ pub async fn verify_hash(file: File, hash: Multihash<256>) -> Res<Option<(u64, M
             "Wrong multihash type {}",
             code
         ))),
-    }?;
+    }
+}
+
+pub async fn verify_hash(file: File, hash: Multihash<256>) -> Res<Option<(u64, Multihash<256>)>> {
+    let file_metadata = file.metadata().await?;
+    let size = file_metadata.len();
+
+    let calculated_hash = refresh_hash(file, &hash).await?;
 
     Ok((hash != calculated_hash).then_some((size, calculated_hash)))
 }
