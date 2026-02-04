9b611af0b83c3ae877fac1ff7b7a1cc5c4a7e323
Maksim Chervonnyi <mail@redmax.dev>
2026-02-04 17:51:54 +0100

    remove ParquetWriter

--- quilt-rs/src/io.rs
+++ quilt-rs/src/io.rs
@@ -1,10 +1,7 @@
 //! Contains helpers and wrappers to work with IO.
 
 pub mod manifest;
-mod parquet;
 /// It is public only for documentation and testing
 pub mod remote;
 /// It is public only for documentation and testing
 pub mod storage;
-
-pub use parquet::ParquetWriter;
